e1c384e015b8febae3e208f2496ed3609e22e3fa
Sanjsigan <sanjsi0507@gmail.com>
2020-09-17 14:14:42 +0530

    create Joke Application and Page in single click

---
+++ JokeWebApplication/JokeWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokeWebApplication.Models;
+
+namespace JokeWebApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokeWebApplication.Models.joke> joke { get; set; }
+    }
+}
